c86adc983ac797bcb757b245ed0ecefe5b436109
joelbits <joak.dev@gmail.com>
2018-02-02 13:28:26 +0700

    Task 6 done

--- mysql/src.sql
+++ mysql/src.sql
@@ -16,5 +16,15 @@ SELECT timestampdiff(SECOND, timestamp(CURDATE()), timestamp(NOW()));
 SELECT * FROM `users` WHERE MONTH(`birthdate`) = 2;
 
 -- Lab 3 - 5 - Skriv en query som visar alla users som har födelsedag idag (om någon).
-SELECT * FROM `users` WHERE DATE(NOW()) = DATE(`birthdate`)
+SELECT * FROM `users` WHERE DATEDIFF(YEAR, DATE(NOW()), DATE(`birthdate`)) > 18
 
+-- Lab 3 - 6 - För users skriv en procedure, young_users(), som som visar förnamn, efternamn och på alla users som är under 18 år (när queryn körs).
+DROP PROCEDURE IF EXISTS young_users;
+DELIMITER //
+CREATE PROCEDURE young_users()
+BEGIN
+SELECT u.fname, u.lname
+FROM users u
+WHERE (SELECT TIMESTAMPDIFF(YEAR, DATE(u.birthdate), DATE(NOW()))) < 18;
+END //
+DELIMITER ;
