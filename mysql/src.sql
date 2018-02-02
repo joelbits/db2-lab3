00d891fe17bd580f7dd0a072907198b2ac7d7a25
joelbits <joak.dev@gmail.com>
2018-02-02 12:49:31 +0700

    Task 4 done

--- mysql/src.sql
+++ mysql/src.sql
@@ -1,14 +1,17 @@
 USE `MYSQL_DATABASE`;
 
 -- Lab 3 - 1 - a - Skriv en query som visar vilken veckodag det kommer vara på den sista dagen nuvarande månad.
-SELECT DAYNAME(LAST_DAY(NOW()))
+SELECT DAYNAME(LAST_DAY(NOW()));
 
 -- Lab 3 - 1 - b - Skriv en query som visar vilken veckordag det kommer vara på den första dagen nästa månad.
-SELECT DAYNAME(LAST_DAY(NOW())+INTERVAL 1 day)
+SELECT DAYNAME(LAST_DAY(NOW())+INTERVAL 1 day);
 
 -- Lab 3 - 2 - Skriv en query som visar hur många dagar det gått sen 2000-01-01.
-SELECT DATEDIFF(NOW(), '2000-01-01')
+SELECT DATEDIFF(NOW(), '2000-01-01');
 
 -- Lab 3 - 3 - Skriv en query som visar hur många minuter det är kvar till midnatt.
-SELECT timestampdiff(SECOND, timestamp(CURDATE()), timestamp(NOW()))
+SELECT timestampdiff(SECOND, timestamp(CURDATE()), timestamp(NOW()));
+
+-- Lab 3 - 4 - Skriv en query som visar alla users som är födda i februari. (users.sql)
+SELECT * FROM `users` WHERE MONTH(`birthdate`) = 2;
 
